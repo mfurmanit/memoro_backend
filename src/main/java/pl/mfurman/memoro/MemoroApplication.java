b8e99d6167ca78ae6e6b7def16849d77d1dee65d
Maciej Furman <maciek.furman.user@gmail.com>
2022-11-01 23:01:47 +0100

    Initial commit.

---
+++ src/main/java/pl/mfurman/memoro/MemoroApplication.java
@@ -0,0 +1,13 @@
+package pl.mfurman.memoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoroApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MemoroApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/mfurman/memoro/MemoroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mfurman.memoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoroApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
